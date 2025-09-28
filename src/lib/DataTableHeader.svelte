f6c3487157888f78db7884e9fd4e6f1d97ae6c3c
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-09-28 10:11:05 +0800

    Add transition to popover

--- src/lib/DataTableHeader.svelte
+++ src/lib/DataTableHeader.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import { fade } from "svelte/transition";
     import filterIcon from "../assets/filter.png";
     const { header } = $props();
+
+    let showFilterPopover = $state(false);
 </script>
 
 <th scope="col" class="px-6 py-3">
@@ -8,12 +11,14 @@
         { header.name }
         {#if header.columnFilter}
             <div class="flex flex-col items-center">
-                <button class="p-1 -mt-1 -mr-5 cursor-pointer rounded-sm hover:bg-gray-200" popovertarget={`filter-popover-${header.name}`} popovertargetaction="toggle">
+                <button class="p-1 -mt-1 -mr-5 cursor-pointer rounded-sm hover:bg-gray-200" popovertarget={`filter-popover-${header.name}`} popovertargetaction="toggle" onclick={() => showFilterPopover = !showFilterPopover}>
                     <img src={filterIcon} aria-label="filter-icon" height={15} width={15} alt="filter-icon"/>
                 </button>
-                <div popover id={`filter-popover-${header.name}`} class="absolute bg-red-100 p-3 rounded-sm overflow-x-auto filter-popover">
-                    {@render header.columnFilter()}
-                </div>
+                {#if showFilterPopover}
+                    <div popover id={`filter-popover-${header.name}`} class="absolute bg-red-100 p-3 rounded-sm overflow-x-auto filter-popover" transition:fade={{ duration: 200 }}>
+                        {@render header.columnFilter()}
+                    </div>
+                {/if}
             </div>
         {/if}
     </div>
