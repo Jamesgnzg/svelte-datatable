e3b11151400ea16a814a8193e2ded25b239b15d4
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-10-20 12:06:11 +0800

    Make cursor pointer on hover of pagination and add gray bg

--- src/lib/DataTableFooter.svelte
+++ src/lib/DataTableFooter.svelte
@@ -23,7 +23,7 @@
                             {/each}
                         </select>
                         <nav class="flex items-center -space-x-px" aria-label="Pagination">
-                            <button type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous"
+                            <button type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 hover:cursor-pointer focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous"
                                 onclick={() => { if( page > 1 ) onPageChange(page - 1)} } disabled={ page <= 1 }>
                                 <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="m15 18-6-6 6-6"></path>
@@ -31,9 +31,9 @@
                                 <span class="hidden sm:block">Previous</span>
                             </button>
                             {#each { length:totalPages } , index }
-                                <button type="button" class="min-h-9.5 min-w-9.5 flex justify-center items-center text-gray-800 border border-gray-200 py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-hidden focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-600 dark:border-neutral-700 dark:text-white dark:focus:bg-neutral-500 { page == index + 1 ? paginationActiveBackgroundColor : '' }" aria-current="page" onclick={() => onPageChange(index + 1)}>{index + 1}</button>
+                                <button type="button" class="min-h-9.5 min-w-9.5 flex justify-center items-center text-gray-800 border border-gray-200 py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg hover:bg-gray-100 hover:cursor-pointer focus:outline-hidden focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-600 dark:border-neutral-700 dark:text-white dark:focus:bg-neutral-500 { page == index + 1 ? paginationActiveBackgroundColor : '' }" aria-current="page" onclick={() => onPageChange(index + 1)}>{index + 1}</button>
                             {/each}
-                            <button type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next"
+                            <button type="button" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 hover:cursor-pointer focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next"
                                 onclick={() => { if( page < totalPages ) onPageChange(page + 1)} } disabled={ page == totalPages }>
                                 <span class="hidden sm:block">Next</span>
                                 <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
